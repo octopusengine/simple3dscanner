e65c8d7bb0389bcff9e60c0670de2624c9a31d7b
Honza S <bonum.comunae@volny.cz>
2016-04-23 12:05:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='scan',
