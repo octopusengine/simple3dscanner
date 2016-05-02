b45a9c42a09ae3f1f61489a5866ca8089bf355b5
Honza S. Copak <bonum.comunae@volny.cz>
2016-05-02 10:55:49 +0200

    Update scannHelp.py

--- scannHelp.py
+++ scannHelp.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python
-# Filename : oeHelp.py
-
+# Filename : scannHelp.py
 #-----------------------------------
 ## simple 3d scanner - octopusengine.eu
 
 def help():
   print ("---------------------------------------")
-  print ("octopusengine/simple3dscanner 0.30 help")
+  print ("octopusengine/simple3dscanner 0.35 help")
   print ("---------------------------------------")
-  print ("scan.py [projectName] [numberOfScann]") 
+  print ("scannStart.py [projectName] [numberOfScann]") 
   print ("Correct input example: scan.py test 100") 
   print ("First argument > project name (default: noname)" )
   print ("Second argument > number of scann (default: 6, for quick test)") 
   print ("---------------------------------------")
-
 
 #---end---
