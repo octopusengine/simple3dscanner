bc359e80888d91f250b1481377d16cf2c5224c70
Honza S. Copak <bonum.comunae@volny.cz>
2016-05-06 18:11:21 +0200

    Update scannHelp.py

--- scannHelp.py
+++ scannHelp.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-# Filename : scannHelp.py
+# Filename : scannHelp.py - 2014/05
 #-----------------------------------
 ## simple 3d scanner - octopusengine.eu
 
 def help():
-  print("----------------------------------------------------------------")
-  print("simple3dscanner 0.35         ---        help       ---          ")
-  print("----------------------------------------------------------------")
+  print("--------------------------------------------------------")
+  print("simple3dscanner 0.35   ---        help       ---        ")
+  print("--------------------------------------------------------")
   print("scannStart.py [projectName] [numberOfScann]") 
   print("Correct input example: scannStart.py test 100") 
   print(">>First argument > "test" => project name" )
@@ -15,7 +15,7 @@ def help():
   print(">>Second argument > 100 => number of scann") 
   print("  100 > one step 360/100 degrees") 
   print("  (default: 6, for quick test)")
-  print("----------------------------------------------------------------")
+  print("--------------------------------------------------------")
   print("(c) https://github.com/octopusengine/simple3dscanner")
 
 #---end---
